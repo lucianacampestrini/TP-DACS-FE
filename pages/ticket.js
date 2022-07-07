d42b8275bdff20c88ede345f6ec28f90bdc3245e
MagaAlaez <maguialaez@gmail.com>
2022-07-07 17:41:09 -0300

    vista espectaculos

---
+++ pages/ticket.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import ContentTicket from "../components/ContentTicket/ContentTicket";
+import Header from "../components/Header/Header";
+import LeftNavbar from "../components/LeftNavbar/LeftNavbar";
+
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>admin</title>
+				<meta name="description" content="Created by streamline" />
+				<link rel="icon" href="/pro.ico" />
+			</Head>
+			<div className={styles.container}>
+				<LeftNavbar />
+				<Header />
+				<ContentTicket />
+			</div>
+		</div>
+	);
+}
